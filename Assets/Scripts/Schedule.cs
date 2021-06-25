6e3b8256a94439f039a48daac765ab7860aebc6e
Rafael Vinueza <rafael.vinueza@epn.edu.ec>
2021-06-24 20:07:38 -0500

    Ya se recibe correctamente la informacion de respuesta del servicio, se guardan los POIs de cada dia de tour

---
+++ Assets/Scripts/Schedule.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+[Serializable]
+public class Schedule
+{
+    public System.Object[] tour;
+    public System.Object[] restaurants;
+}
---
+++ Assets/Scripts/Tour.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class Tour
+{
+    private List<PointInterest>[] days;
+
+    public List<PointInterest>[] Days { get => days; set => days = value; }
+
+    public Tour()
+    {
+    }
+}
