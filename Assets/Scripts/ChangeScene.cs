ed0556e6ac490570307419efc9117802fff73d70
Renato <burbano.renato@gmail.com>
2021-06-29 20:21:56 -0500

    Creada escena Loading y transición entre escenas

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -5,10 +5,38 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
+    Validations validations;
 
-    public void LoadScene(string sceneName)
+    private void Awake()
     {
-        SceneManager.LoadScene(sceneName);
+        if(SceneManager.GetActiveScene().name == "SceneForm")
+            validations = GameObject.Find("ButtonSubmit").GetComponent<Validations>();
+    }
+
+    public void LoadScene()
+    {
+        string sceneActual = SceneManager.GetActiveScene().name;
+        switch (sceneActual)
+        {
+            case "SceneForm":
+                if (validations.validate())
+                {
+                    SceneManager.LoadScene("SceneLoading");
+                }
+                else
+                {
+                    //devolver los 
+                }
+                break;
+            case "Scene1":
+                SceneManager.LoadScene("Scene2");
+                break;
+            case "Scene2":
+                SceneManager.LoadScene("Scene1");
+                break;
+        }
+
+        //SceneManager.LoadScene(sceneName);
     }
 
 }
